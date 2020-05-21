b3407eb1fd94643e4c33fc2908c8e7e177f3a0d9
Sjika <sjika.lt@gmail.com>
2020-05-21 22:59:34 +0800

    optimize webpack entry chain

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       chainWebpackMainProcess: config => {
-        const _ = config.entryPoints.store.get("background");
-        config.entryPoints.store.set("client", _);
-        config.entryPoints.store.delete("background");
+        config.entryPoints.set("client", config.entry("background")).delete("background");
       },
       mainProcessFile: "./src/client.js"
     }
